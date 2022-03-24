a2742ddcac75d231f0a08c187951c472e4fd99ee
Colby Schulz <colbyschulz@Colbys-MBP.lan>
2022-03-23 23:25:00 -0400

    revert to building dist assets in root in support of github pages constraints

--- webpack.config.js
+++ webpack.config.js
@@ -10,9 +10,9 @@ module.exports = {
     extensions: ['.ts', '.tsx', '.js'],
   },
   output: {
-    path: path.join(__dirname, '/dist'),
+    path: path.join(__dirname, '/'),
     filename: 'bundle.js',
-    publicPath: '/dist',
+    publicPath: '/',
   },
   module: {
     rules: [
