b8f9cebd150386e9385b5ab8dc898f505a1538f5
Colby Schulz <colbyschulz@Colbys-MBP.lan>
2022-03-23 23:03:57 -0400

    index.html in root

--- webpack.config.js
+++ webpack.config.js
@@ -10,7 +10,7 @@ module.exports = {
     extensions: ['.ts', '.tsx', '.js'],
   },
   output: {
-    path: path.join(__dirname, '/dist'),
+    path: path.join(__dirname, '/'),
     filename: 'bundle.js',
     publicPath: '/',
   },
