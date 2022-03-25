36cf634f66ed3ac1923ec144c038a27f5973a494
Anne Bozack <anne.bozack@gmail.com>
2022-03-25 12:50:52 -0400

    Update AboutPage.tsx

--- src/pages/aboutPage/AboutPage.tsx
+++ src/pages/aboutPage/AboutPage.tsx
@@ -2,11 +2,7 @@ import React, { FC } from 'react';
 import { AboutPageWrapper } from './aboutPage.css';
 
 const AboutPage: FC = () => {
-  return <AboutPageWrapper>Anne Bozack is a postdoctoral fellow in the <a href="https://publichealth.berkeley.edu/research/cardenas-lab/">Cardenas lab</a> in the Division of 
-       Environmental Health Sciences in the University of California, Berkeley School of Public Health. Her research focuses on the impact of environmental 
-       exposures on the molecular level and effects of nutrition. Her current projects investigate the associations between chronic exposure to metals and DNA 
-       methylation. Anne holds a Ph.D. in Environmental Health Sciences and M.P.H. in Sociomedical Sciences from Columbia University, and B.A. degrees in 
-  Environmental Science and Architecture from the University of California, Berkeley.</AboutPageWrapper>;
+  return <AboutPageWrapper>About Page</AboutPageWrapper>;
 };
 
 export default AboutPage;
