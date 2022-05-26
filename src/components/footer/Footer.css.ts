741823e06200b815710d360f6d977e73860ba1d9
Colby Schulz <colbyschulz@Colbys-MBP.lan>
2022-05-26 18:34:19 -0400

    un sticky footer

--- src/components/footer/Footer.css.ts
+++ src/components/footer/Footer.css.ts
@@ -2,11 +2,8 @@ import styled from 'styled-components';
 
 export const FooterWrapper = styled.div`
   display: flex;
-  position: fixed;
-  bottom: 0;
   align-items: center;
   justify-content: flex-start;
-  position: sticky;
   background: white;
   padding: 20px;
   border-top: 3px solid black;
