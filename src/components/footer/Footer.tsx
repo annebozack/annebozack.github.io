2a29b6e70aef47a28155bfb3e201ff867b0b5173
Colby Schulz <colbyschulz@Colbys-MBP.lan>
2022-05-26 18:30:13 -0400

    new tab footer links

--- src/components/footer/Footer.tsx
+++ src/components/footer/Footer.tsx
@@ -5,11 +5,21 @@ const Footer: FC = () => {
   return (
     <FooterWrapper>
       {' '}
-      <StyledLink href="https://github.com/annebozack">Github</StyledLink>
-      <StyledLink href="https://scholar.google.com/citations?user=kSzZe7QAAAAJ&hl=en">Google Scholar </StyledLink>
-      <StyledLink href="https://www.linkedin.com//inStyledLinknnebozack/">LinkedIn </StyledLink>
-      <StyledLink href="https://pubmed.ncbi.nlm.nih.gov/?term=anne+bozack&sort=date">PubMed </StyledLink>
-      <StyledLink href="https://osf.io/c4ghz/">OSF </StyledLink>
+      <StyledLink target="_blank" href="https://github.com/annebozack">
+        Github
+      </StyledLink>
+      <StyledLink target="_blank" href="https://scholar.google.com/citations?user=kSzZe7QAAAAJ&hl=en">
+        Google Scholar{' '}
+      </StyledLink>
+      <StyledLink target="_blank" href="https://www.linkedin.com//inStyledLinknnebozack/">
+        LinkedIn{' '}
+      </StyledLink>
+      <StyledLink target="_blank" href="https://pubmed.ncbi.nlm.nih.gov/?term=anne+bozack&sort=date">
+        PubMed{' '}
+      </StyledLink>
+      <StyledLink target="_blank" href="https://osf.io/c4ghz/">
+        OSF{' '}
+      </StyledLink>
     </FooterWrapper>
   );
 };
